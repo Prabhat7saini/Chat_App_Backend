334cdc863ebeeb8273de970875cd95d7950fcb96
Prabhat Saini <Prabhat.saini@binmile.com>
2024-08-20 11:48:27 +0530

    inital commit

---
+++ config/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: "dpocsv31b",
+  api_key: "553699792243314",
+  api_secret: "WI8DxxgmW05C0ltQWZ1Ae0jDdKs", // Click 'View Credentials' below to copy your API secret
+});
+
+module.exports = cloudinary;
